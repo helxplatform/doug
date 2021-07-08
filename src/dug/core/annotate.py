eeb53b3998f47fac1e440eb17368970f7dec27cb
warrenstephens <wstephens@renci.org>
2021-07-08 16:10:37 -0400

    log response text

--- src/dug/core/annotate.py
+++ src/dug/core/annotate.py
@@ -304,9 +304,9 @@ class Normalizer(ApiClient[Identifier, Identifier]):
     def make_request(self, value: Identifier, http_session: Session) -> dict:
         curie = value.id
         url = f"{self.url}{urllib.parse.quote(curie)}"
-        response = http_session.get(url)
-        logger.debug(f"make_request^response: {len(response)} {response}")
-        normalized = response.json() # normalized = http_session.get(url).json()
+        response = http_session.get(url).text
+        logger.debug(f"make_request^response: {len(response)} '{response}'")
+        normalized = json.loads(response) # normalized = http_session.get(url).json()
         return normalized
 
     def handle_response(self, identifier: Identifier, normalized: dict) -> Identifier:
